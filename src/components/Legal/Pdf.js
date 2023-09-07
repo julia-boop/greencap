8b6b87e54e35eeb9b27d3755094ceae41d516a14
julia-boop <crdr.jc@gmail.com>
2023-09-06 22:09:27 -0300

    warnings fix

--- src/components/Legal/Pdf.js
+++ src/components/Legal/Pdf.js
@@ -5,25 +5,25 @@ function Pdf() {
         <div className="pdf-container">
             <div className="pdf-item">
                 <div className="pdf-item-img">
-                    <img src="/items/cnv.png"></img>
+                    <img src="/items/cnv.png" alt="cnv"></img>
                 </div>
                 <a href="/pdf/Licencia.pdf" download><p>Inscripción en la CNV</p></a>
             </div>
             <div className="pdf-item">
                 <div className="pdf-item-img">
-                    <img src="/items/logo2.png"></img>
+                    <img src="/items/logo2.png" alt="logo"></img>
                 </div>
                 <a href="/pdf/CodigoEtica.pdf" download><p>Código de ética</p></a>
             </div>
             <div className="pdf-item">
                 <div className="pdf-item-img">
-                    <img src="/items/bva.png"></img>
+                    <img src="/items/bva.png" alt="bva"></img>
                 </div>
                 <a href="/pdf/InscripcionBVA.pdf" download><p>Inscripción en la Bolsa de Valores de Asunción</p></a>                
             </div>
             <div className="pdf-item">
                 <div className="pdf-item-img">  
-                    <img src="/items/bva.png"></img><img></img>
+                    <img src="/items/bva.png" alt="bva"></img>
                 </div>
                 <a href="/pdf/AccionBVA.pdf" download><p>Acción de la Bolsa de Valores de Asunción</p></a>
             </div>
